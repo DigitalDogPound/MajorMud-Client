c784879da417f4bad2b8c70f5aeeaf61477a00cc
Arisance <2018609d@opayq.com>
2022-06-27 21:40:34 -0500

    Start Form1 on app load

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -16,11 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new main());
+            Application.Run(new Form1());
         }
-    }
-
-    internal class main : Form
-    {
     }
 }
